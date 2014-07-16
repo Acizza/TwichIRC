83cc39d7ae35c139cbb0f3ad72ba27948c44138c
unknown-user <unknown-user@192.168.8.101>
2014-07-16 16:28:23 -0700

    Initial check-in of module TwitchIRC

---
+++ TwitchIRC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TwitchIRC
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var parser = new ArgParser();
+
+			parser.Parse(args);
+		}
+	}
+}
---
+++ TwitchIRC/Util/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TwitchIRC
+{
+	public static class Log
+	{
+		public static void Info(string msg)
+		{
+			Console.WriteLine("INFO: " + msg);
+		}
+
+		public static void Warning(string msg)
+		{
+			Console.WriteLine("WARNING: " + msg);
+		}
+
+		public static void Error(string msg)
+		{
+			Console.WriteLine("ERROR: " + msg);
+		}
+	}
+}
