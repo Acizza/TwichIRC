6487e4f7008ec625e8964949470be6d3bd019659
Acizza <killertaco93@gmail.com>
2014-12-20 05:12:41 -0800

    Added additional library functionality.
    * Extensions.cs:
      Added Range() method.
    
    * IRCClient.cs:
      Added LoggedIn field.
      Added Username field.
      Added MaxBufferSize field.
      Added VerifyLogin() method.
      Added overloaded Connect() method that will also login a user.
      Added SendLine(), ReadLine(), Logout(), and Dispose() methods.
      Implemented Login() method.

--- Twirc.Lib/Source/IRCClient.cs
+++ Twirc.Lib/Source/IRCClient.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Text;
+using Twirc.Lib.Util;
 
 namespace Twirc.Lib
 {
-	public sealed class IRCClient
+	public sealed class IRCClient : IDisposable
 	{
-		public enum LoginStatus
-		{
-			Success,
-			Failed,
-			NotConnected
-		}
-
 		/// <summary>
 		/// The socket used for sending and receiving data from the server.
 		/// </summary>
 		/// <value>The socket.</value>
 		public Socket Socket { get; private set; }
+
+		/// <summary>
+		/// A value indicating if the client is logged in to the connected server.
+		/// </summary>
+		/// <value><c>true</c> if logged in; otherwise, <c>false</c>.</value>
+		public bool LoggedIn { get; private set; }
+
+		/// <summary>
+		/// The username currently logged in.
+		/// </summary>
+		/// <value>The username.</value>
+		public string Username { get; private set; }
+
+		/// <summary>
+		/// The maximum amount of bytes to read from the server.
+		/// </summary>
+		public uint MaxBufferSize = 512;
 
 		/// <summary>
 		/// The host address being used. When set, <see cref="Connect"/> is called with the new information.
@@ -67,6 +79,52 @@ namespace Twirc.Lib
 		string _host;
 		int _port;
 
+		public IRCClient()
+		{
+		}
+
+		public IRCClient(string host, int port)
+		{
+			Connect(host, port);
+		}
+
+		public IRCClient(string host, int port, string username, string password)
+		{
+			Connect(host, port, username, password);
+		}
+
+		~IRCClient()
+		{
+			Dispose();
+		}
+
+		/// <summary>
+		/// Checks any codes sent by the server to see if a login attempt was successful.
+		/// </summary>
+		/// <returns><c>true</c>, if login was successful, <c>false</c> otherwise.</returns>
+		bool VerifyLogin()
+		{
+			string data;
+
+			while((data = ReadLine()) != null)
+			{
+				foreach(var line in data.Split('\n'))
+				{
+					string code = line.Range(" ");
+
+					if(code.Length == 0)
+						continue;
+
+					if(code == "004" || code == "375")
+						return true;
+					if(code[0] == '5' || code[0] == '4' || code == "NOTICE")
+						return false;
+				}
+			}
+
+			return false;
+		}
+
 		/// <summary>
 		/// Connects to the specified server.
 		/// </summary>
@@ -74,13 +132,26 @@ namespace Twirc.Lib
 		/// <param name="port">Port.</param>
 		public void Connect(string host, int port)
 		{
-			Close();
+			Dispose();
 
 			Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 			Socket.Connect(host, port);
 
 			_host = host;
 			_port = port;
+		}
+
+		/// <summary>
+		/// Connects to the specified server and logins the specified user.
+		/// </summary>
+		/// <param name="host">Host.</param>
+		/// <param name="port">Port.</param>
+		/// <param name="username">Username.</param>
+		/// <param name="password">Password.</param>
+		public void Connect(string host, int port, string username, string password)
+		{
+			Connect(host, port);
+			Login(username, password);
 		}
 
 		/// <summary>
@@ -92,32 +163,79 @@ namespace Twirc.Lib
 		{
 			if(!Alive)
 				return LoginStatus.NotConnected;
+				
+			Username = username;
+			LoggedIn = false;
 
-			// TODO: Finish
+			SendLine("PASS " + password);
+			SendLine("NICK " + username);
+			SendLine("USER " + username + " 8 * :" + username);
 
+			if(!VerifyLogin())
+				return LoginStatus.Failed;
+
+			LoggedIn = true;
 			return LoginStatus.Success;
 		}
 
-		public void Disconnect()
+		/// <summary>
+		/// Sends a line of data to the server. Automatically appends \n.
+		/// </summary>
+		/// <param name="data">Data to send.</param>
+		public void SendLine(string data)
 		{
 			if(!Alive)
 				return;
 
+			Socket.Send(Encoding.UTF8.GetBytes(data + '\n'));
+		}
+
+		/// <summary>
+		/// Reads the next line from the server.
+		/// </summary>
+		/// <returns>The line.</returns>
+		public string ReadLine()
+		{
+			if(!Alive)
+				return "";
+
+			var buffer = new byte[MaxBufferSize];
+			int read   = Socket.Receive(buffer);
+
+			return Encoding.UTF8.GetString(buffer, 0, read);
+		}
+
+		/// <summary>
+		/// Logs out from the server.
+		/// </summary>
+		public void Logout()
+		{
+			if(!Alive || !LoggedIn)
+				return;
+
+			SendLine("QUIT :Logout");
 			// TODO: Finish
 		}
 
 		/// <summary>
 		/// Disconnects from the server and closes the socket.
 		/// </summary>
-		public void Close()
+		public void Dispose()
 		{
 			if(!Alive)
 				return;
 
-			Disconnect();
+			Logout();
 
 			Socket.Shutdown(SocketShutdown.Both);
 			Socket.Close();
 		}
+	}
+
+	public enum LoginStatus
+	{
+		Success,
+		Failed,
+		NotConnected
 	}
 }
---
+++ Twirc.Lib/Source/Util/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Twirc.Lib.Util
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Returns the string found between start and end. Returns an empty string if nothing is found.
+		/// </summary>
+		/// <param name="source">Source string.</param>
+		/// <param name="start">Start string.</param>
+		/// <param name="end">End string.</param>
+		/// <param name="offset">Offset to start looking at.</param>
+		public static string Range(this string source, string start, string end, int offset = 0)
+		{
+			int startIdx = source.IndexOf(start, offset);
+
+			if(startIdx < 0)
+				return "";
+
+			int endIdx = source.IndexOf(end, startIdx + offset + 1); // Add 1 to skip the start index in case they're the same
+
+			if(endIdx < 0)
+				return "";
+
+			return source.Substring(startIdx + start.Length, endIdx - startIdx - end.Length);
+		}
+
+		/// <summary>
+		/// Returns the string found between start. Returns an empty string if nothing is found.
+		/// </summary>
+		/// <param name="source">Source string.</param>
+		/// <param name="start">Start string.</param>
+		/// <param name="offset">Offset to start looking at.</param>
+		public static string Range(this string source, string start, int offset = 0)
+		{
+			return Range(source, start, start, offset);
+		}
+	}
+}
