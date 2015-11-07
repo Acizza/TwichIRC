088236db1987be3f1d6a8d28834c8bf37678f79a
Acizza <killertaco93@gmail.com>
2015-11-06 23:30:42 -0800

    Fix exception on an empty socket read. Improve stability

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,19 @@
 module Main where
 
 import Client
+import Control.Exception (finally)
 import Control.Monad (forever, unless)
 import System.IO
 
-startMessageLoop :: Handle -> IO ()
-startMessageLoop handle = forever $ do
-    line <- hGetLine handle
-    unless (null line) $ putStrLn line
+processNetworkLoop :: Handle -> IO ()
+processNetworkLoop handle = do
+    eof <- hIsEOF handle
+    unless eof $ do
+        line <- hGetLine handle
+        unless (null line) $ putStrLn line
+        processNetworkLoop handle
 
 main :: IO ()
 main = do
     handle <- connect "irc.twitch.tv" 6667
-    login "" "" handle
-        >>= startMessageLoop
-    hClose handle
+    (login "" "" handle >>= processNetworkLoop) `finally` hClose handle
