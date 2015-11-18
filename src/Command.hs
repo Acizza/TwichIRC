2ce62faf5f5f2194e9d340b70f2d07ab9c1da7b3
Acizza <killertaco93@gmail.com>
2015-11-18 00:46:53 -0800

    Cleanup command processor. Add leave & "commands" command

--- src/Command.hs
+++ src/Command.hs
@@ -3,7 +3,8 @@ module Command (process) where
 import Control.Monad (foldM)
 import Data.List (find)
 import Text.Printf (printf)
-import IRC.Client (State, joinChannel)
+import IRC.Display (printCC)
+import IRC.Client (State, joinChannel, leaveChannel)
 
 type Name = String
 type Usage = String
@@ -15,22 +16,36 @@ type Command = (Name, Usage, NumArgs, Action)
 
 commands :: [Command]
 commands = [
-    ("join", "<channels>", 1, joinCmd)]
+    ("join", "<channels>", 1, joinCmd),
+    ("leave", "<channels>", 1, leaveCmd)]
 
-joinCmd :: State -> Arguments -> IO State
 joinCmd = foldM (flip joinChannel)
+leaveCmd = foldM (flip leaveChannel)
+
+printCommands :: IO ()
+printCommands =
+    mapM_ (\(name, usage, _, _) ->
+        printCC $ printf "~r~%s: ~w~%s\n" name usage)
+        commands
 
 findCommand :: Name -> Maybe Command
 findCommand name = find (\(n, _, _, _) -> n == name) commands
 
+executeCommand :: Name -> Arguments -> State -> Either String (IO State)
+executeCommand name args state =
+    case findCommand name of
+        Just (_, usage, nArgs, f) ->
+            if nArgs > length args
+            then Left $ printf $ "Usage: " ++ usage
+            else Right $ f state args
+        Nothing -> Left $ "Command not found: " ++ name
+
 process :: State -> String -> Either String (IO State)
 process state = match . words
     where
-        match (cmd:args) =
-            case findCommand cmd of
-                Just (_, usage, reqArgs, f) ->
-                    if reqArgs > length args
-                    then Left $ printf $ "Usage: " ++ usage
-                    else Right $ f state args
-                Nothing -> Left $ "Command not found: " ++ cmd
+        match ("commands":args) =
+            Right $ do
+                printCommands
+                return state
+        match (cmd:args) = executeCommand cmd args state
         match [] = Left "No input"
